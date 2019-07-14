f8478d19265d203513c55746a8d0b42cd99299ca
Kenney Knisely <kknisely@mac.com>
2019-07-14 19:29:33 -0400

    Add first variables, functions, and event listener

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // Global Variables
+const myBoxes = document.getElementsByClassName('block');
 
 
 // The Functionality
+function loopThrough(){
+  console.log(this);
+};
 
 
 // The Event Listener
+for(let i = 0; i < myBoxes.length; i++ ){
+  myBoxes[i].addEventListener('click', loopThrough);
+};
